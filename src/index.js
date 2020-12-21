0e9c7ddda3d156b4369521025ce30fd94317124d
Orin Fletcher <orinf@yahoo.com>
2020-12-20 20:35:16 -0800

    adds images

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const image = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
     <h1 className="heading" spellCheck="false">
       My Favorite Foods
     </h1>
     <ul>
-      <li>Bacon</li>
-      <li>Sushi</li>
-      <li>Gummy Bears</li>
+      <img src={image} alt="100x100" className="sqr-img" />
+      <img src={image} alt="100x100" className="sqr-img" />
+      <img src={image} alt="100x100" className="sqr-img" />
     </ul>
   </div>,
   document.getElementById("root")
